f6e318c78812040c0f3847d819149c3ac91ee3d4
github takaryu39 <ryuta0takamiya@gmail.com>
2024-03-30 20:46:42 +0900

    add prisma & zod

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+app.get("/entry/:id", (c) => {
+  return c.json({ "your id": c.req.param("id") });
+});
 
-export default app
+export default app;
